0d2f285108cd1f2830922721965e37ffd5cf325d
IsaqueMansur <isaquemansur@gmail.com>
2023-05-08 10:55:14 -0300

    serialization and reverting proccess

---
+++ SerializaçãoEDesserizalização/SerializaçãoEDesserizalização/Dog.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace SerializaçãoEDesserizalização
+{
+    [DataContract]
+    internal class Dog
+    {
+        [DataMember]
+        public string name;
+        [DataMember]
+        public string color;
+
+        public Dog(string name, string color) 
+        {
+            this.name = name;
+            this.color = color;
+        }
+        public Dog()
+        {
+
+        }
+
+    }
+}
---
+++ SerializaçãoEDesserizalização/SerializaçãoEDesserizalização/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.IO;
+using System.Runtime.Serialization;
+
+namespace SerializaçãoEDesserizalização
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog myDog = new Dog("Bob", "Black");
+            Dog otherDog;
+
+            DataContractSerializer serializer = new DataContractSerializer(typeof(Dog));
+
+            //Serialização
+            XmlWriterSettings xmlConfig = new XmlWriterSettings { Indent = true };
+            StringBuilder constructorOfString = new StringBuilder();
+            XmlWriter xmlWriter = XmlWriter.Create(constructorOfString, xmlConfig);
+            serializer.WriteObject(xmlWriter, myDog);
+            xmlWriter.Flush();
+            string serializedObject = constructorOfString.ToString();
+
+            //Saving in folder using xml format
+            string path = "dog.xml";
+            FileStream myFileXml = File.Create(path);
+            myFileXml.Close();
+            File.WriteAllText(path, serializedObject);
+
+            //Desserialização
+            string contentOfXml = File.ReadAllText(path);
+            StringReader readerOfStrings = new StringReader(contentOfXml);
+            XmlReader readerOfXml = XmlReader.Create(readerOfStrings);
+            otherDog = (Dog)serializer.ReadObject(readerOfXml);
+
+            readerOfXml.Close();
+            xmlWriter.Close();
+        }
+    }
+}
