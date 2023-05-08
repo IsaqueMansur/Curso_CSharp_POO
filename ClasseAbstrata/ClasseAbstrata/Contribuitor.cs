269e5341c3fda4968633e5d23cdd5e7ce51698cf
IsaqueMansur <isaquemansur@gmail.com>
2023-05-08 09:21:19 -0300

    Abstract class

---
+++ ClasseAbstrata/ClasseAbstrata/Contribuitor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClasseAbstrata
+{
+    internal class Contribuitor: People
+    {
+        private UInt32 wage;
+
+        public Contribuitor(string name, UInt32 wage) : base(name)
+        {
+            this.wage = wage;
+        }
+
+        public UInt32 Wage
+        {
+            get { return wage; }
+            set { this.wage = value; }
+        }
+
+        public void ReadWage()
+        {
+            Console.WriteLine("O salário do funcionário " + Name  + " é: " + this.wage);
+        }
+    }
+}
---
+++ ClasseAbstrata/ClasseAbstrata/People.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClasseAbstrata
+{
+    internal abstract class People
+    {
+        private string name;
+
+        public People(string name)
+        {
+            this.name = name;
+        }
+
+        public string Name 
+        { 
+            get { return name; } 
+            set { this.name = value; } 
+        } 
+
+        public void ReadName()
+        {
+            Console.WriteLine("O nome da pessoa é: " + this.name);
+        }
+
+    }
+}
